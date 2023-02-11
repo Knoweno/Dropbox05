56f3eaf21d83b8c79cda936f487472358ae57400
HP <HP@DESKTOP-B9LCRK2>
2023-02-10 23:04:03 -0500

    Add project files.

---
+++ Dropbox05/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dropbox05.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Employee/AddEmployee 
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+        //Employee/AllEmployee 
+        public ActionResult AllEmployee()
+        {
+            return View();
+        }
+    }
+}
